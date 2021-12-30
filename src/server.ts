0269274af02d06bd9f946f2cc28b0d4519e11779
danymat <d.danymat@gmail.com>
2021-12-30 11:23:25 +0100

    feat: Add session/start method

--- src/server.ts
+++ src/server.ts
@@ -2,47 +2,55 @@ import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import { Session, User, UserDocument } from "./types";
 import {
-    isJSONRPCResponse,
-    JSONRPCClient,
     JSONRPCResponse,
     JSONRPCServer,
-    JSONRPCServerAndClient,
 } from "json-rpc-2.0";
-
-// TODO: Remove this
-
-let testDocument: UserDocument = {
-    filename: "testDocument",
-    uuid: "1111",
-};
-
-let testMaster: User = {
-    uuid: "2222",
-    username: "danymat",
-    ephemeralIdentity: "1234",
-    identityProof: "2345",
-};
-
-let session: Session = {
-    uuid: "3333",
-    master: testMaster,
-    documents: [testDocument],
-};
 
 const server = new JSONRPCServer();
 
-server.addMethod("echo", (text) => text);
-server.addMethod("log", (message) => console.log(message));
+let sessions = [];
+
+server.addMethod("session/start", (params: any[]) => {
+    if (params.length != 3) {
+        console.log("err");
+        return;
+    }
+
+    let master: User = {
+        uuid: "1",
+        username: "test",
+        ephemeralIdentity: params[2],
+        identityProof: params[3],
+    };
+
+    let document: UserDocument = {
+        filename: params[1],
+        uuid: "1",
+    };
+
+    let session: Session = {
+        uuid: "1",
+        master: master,
+        documents: [document],
+    };
+
+    sessions.push(session)
+
+
+});
+server.addMethod("session/join", () => {
+    return "hey";
+});
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/json-rpc", (req, res) => {
+app.post("/json-rpc", (req: Request, res: Response) => {
     const jsonRPCRequest = req.body;
     // server.receive takes a JSON-RPC request and returns a promise of a JSON-RPC response.
     // It can also receive an array of requests, in which case it may return an array of responses.
     // Alternatively, you can use server.receiveJSON, which takes JSON string as is (in this case req.body).
-    server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
+    server.receive(jsonRPCRequest).then((jsonRPCResponse: JSONRPCResponse) => {
         if (jsonRPCResponse) {
             res.json(jsonRPCResponse);
         } else {
